d69216dd0fe5e871f6b0e5d381b5b2fd9e572a0c
Marcin <marcin96semkowski@gmail.com>
2019-11-07 19:04:36 +0100

    Animation #3

--- app/src/main/java/animations/com/MainActivity.java
+++ app/src/main/java/animations/com/MainActivity.java
@@ -10,19 +10,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void fade(View view){
         ImageView morty = findViewById(R.id.Morty);
-        ImageView rick = findViewById(R.id.Rick);
+        //ImageView rick = findViewById(R.id.Rick);
 
-        morty.animate().alpha(0f).setDuration(2000);
+        morty.animate().translationXBy(1000f).setDuration(2000);
 
-        rick.animate().alpha(1f).setDuration(2000);
-    }
 
-    public void fadeRick(View view){
-        ImageView rick = findViewById(R.id.Rick);
-        ImageView morty = findViewById(R.id.Morty);
-        rick.animate().alpha(0f).setDuration(2000);
-        morty.animate().alpha(1f).setDuration(2000);
-
+        //rick.animate().alpha(1f).setDuration(2000);
     }
 
 
@@ -34,5 +27,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView morty = findViewById(R.id.Morty);
+        morty.setTranslationX(-1000f);
+
     }
 }
