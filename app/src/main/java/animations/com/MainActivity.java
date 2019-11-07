7ef17412671dfe9f1dee84e4cb6fedc6efb99eaa
Marcin <marcin96semkowski@gmail.com>
2019-11-07 18:24:10 +0100

    Update

--- app/src/main/java/animations/com/MainActivity.java
+++ app/src/main/java/animations/com/MainActivity.java
@@ -3,8 +3,19 @@ package animations.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+        ImageView morty = findViewById(R.id.Morty);
+
+        morty.animate().alpha(0f).setDuration(2000);
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
