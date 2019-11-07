17896e90dde163f38d7ef1e903407f3a902a7121
Marcin <marcin96semkowski@gmail.com>
2019-11-07 18:48:41 +0100

    Animation #2

--- app/src/main/java/animations/com/MainActivity.java
+++ app/src/main/java/animations/com/MainActivity.java
@@ -15,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
         morty.animate().alpha(0f).setDuration(2000);
 
         rick.animate().alpha(1f).setDuration(2000);
+    }
 
-
+    public void fadeRick(View view){
+        ImageView rick = findViewById(R.id.Rick);
+        ImageView morty = findViewById(R.id.Morty);
+        rick.animate().alpha(0f).setDuration(2000);
+        morty.animate().alpha(1f).setDuration(2000);
 
     }
+
+
+
 
 
 
